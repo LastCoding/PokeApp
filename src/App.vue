cf799cb245fb181e1bdf01aec4d330fc86d5d8bf
LastFit <inyakimd@gmail.com>
2021-07-24 23:00:50 +0200

    SPA Basics Done

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/pokemon-logo.png" width="250" height="250">
-    <HelloWorld msg="Bienvenido a la PokeApp"/>
+    <img alt="Vue logo" src="./assets/images/pokemon-logo.png" width="250" height="250">
     <PokeForm></PokeForm>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import PokeForm from './components/PokeForm.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     PokeForm
   }
 }
--- src/components/PokeForm.vue
+++ src/components/PokeForm.vue
@@ -5,7 +5,7 @@
     <div id="wait">
       <hr />
       <h2>
-        Por favor, espere... tratar con una API, a veces puede demorar un poco.
+        Por favor, espere... tratar con una API, le rogamos paciencia, a veces puede demorar un poco.
       </h2>
       <img src="../assets/images/working.gif" />
     </div>
@@ -41,7 +41,7 @@ export default {
       totalPokemons: null,
       info: null,
       uniqueEncounter: [],
-      pokemonsInfo: [],
+      pokemonsInfo: []
     };
   },
   methods: {
@@ -50,10 +50,10 @@ export default {
         const resp = await axios.get(apiURL);
         this.info = resp;
       } catch (err) {
+        // If there's 404 error, It means there's no more pokemon to show, so I stop the loop.
         if (err.response && err.response.status === 404) {
           console.error(err);
           this.totalPokemons = 1;
-          // If there's an 404 error, It means there's no more pokemon to show, so I stop the loop.
         }
       }
     },
@@ -69,6 +69,7 @@ export default {
     getPokemonList: async function () {
       try {
         document.getElementById("wait").style.display = "inline";
+        // We get a list of all pokemons, and filter by who have only one encounter
         for (var i = 1; i < this.totalPokemons; i++) {
           apiURL = "https://pokeapi.co/api/v2/pokemon/" + i + "/encounters";
           await this.sendGetRequest();
@@ -89,6 +90,7 @@ export default {
     getPokemonNameAndType: async function () {
       try {
         let pokemon = {};
+        // Here It's collected the Name & Type from each pokemon to an array of objects 
         for (var i = 0; i < this.uniqueEncounter.length; i++) {
           apiURL =
             "https://pokeapi.co/api/v2/pokemon/" + this.uniqueEncounter[i];
@@ -103,14 +105,13 @@ export default {
           this.pokemonsInfo.push(pokemon);
           pokemon = {};
         }
-        document.getElementById("wait").style.display = "none";
-        document.getElementById("pokemonList").style.display = "inline";
       } catch (err) {
         console.error(err);
       }
     },
     getPokemonGeneration: async function () {
       try {
+        // Here It's collected the info of from which generation is each pokemon who have only one encounter
         for (var x = 0; x < this.pokemonsInfo.length; x++) {
           for (var i = 1; i < 9; i++) {
             apiURL = "https://pokeapi.co/api/v2/generation/" + i;
@@ -122,17 +123,17 @@ export default {
             if (tempVar === undefined || tempVar.length === 0) {
               continue;
             } else {
-              this.pokemonsInfo[x].generation = i.toString();
+              this.pokemonsInfo[x].generation = i;
               break;
             }
           }
         }
-        console.log(this.pokemonsInfo);
       } catch (err) {
         console.error(err);
       }
     },
     checkNameTypos: async function () {
+      // Here It's a cleaning of data, because API had some typos
       let tempSplittedName;
       for (var i = 0; i < this.pokemonsInfo.length; i++) {
         if (this.pokemonsInfo[i].name.includes("-normal")) {
@@ -142,23 +143,62 @@ export default {
       }
     },
     splitTypes: async function () {
+      // Here I split the types between 2, to a correct sort & show later 
       for (var i = 0; i < this.pokemonsInfo.length; i++) {
-          this.pokemonsInfo[i].type1 = this.pokemonsInfo[i].types[0];
+        this.pokemonsInfo[i].type1 = this.pokemonsInfo[i].types[0];
         if (this.pokemonsInfo[i].types.length > 1) {
           this.pokemonsInfo[i].type2 = this.pokemonsInfo[i].types[1];
         }
       }
     },
-    showTable: async function () {
+    filterAndSortPokemons: async function () {
+      let sortedGenerations1 = [];
+      let sortedGenerations2 = [];
+      let sortedGenerations3 = [];
 
+      // Sort by Type
+      this.pokemonsInfo.sort((a, b) => a.type1.localeCompare(b.type1));
+
+      // Cloning generations sorted
+      for (var i = 0; i < this.pokemonsInfo.length; i++) {
+        switch (this.pokemonsInfo[i].generation) {
+          case 1:
+            sortedGenerations1.push(this.pokemonsInfo[i]);
+            break;
+
+          case 2:
+            sortedGenerations2.push(this.pokemonsInfo[i]);
+            break;
+
+          case 3:
+            sortedGenerations3.push(this.pokemonsInfo[i]);
+            break;
+        }
+      }
+      this.pokemonsInfo = [];
+
+      // Copy to the final array sorted
+      for (var a = 0; a < sortedGenerations1.length; a++) {
+        this.pokemonsInfo.push(sortedGenerations1[a]);
+      }
+      for (var e = 0; e < sortedGenerations2.length; e++) {
+        this.pokemonsInfo.push(sortedGenerations2[e]);
+      }
+      for (var o = 0; o < sortedGenerations3.length; o++) {
+        this.pokemonsInfo.push(sortedGenerations3[o]);
+      }
+      document.getElementById("wait").style.display = "none";
+      document.getElementById("pokemonList").style.display = "inline";
     },
     showPokemon: async function () {
+      // Here It's the order of priority for each method to execute
       await this.getPokemonCounter();
       await this.getPokemonList();
       await this.getPokemonNameAndType();
       await this.checkNameTypos();
       await this.getPokemonGeneration();
       await this.splitTypes();
+      await this.filterAndSortPokemons();
     },
   },
 };
